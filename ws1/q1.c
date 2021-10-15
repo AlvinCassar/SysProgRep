ee894adc81b6e3912b785dcee5e3e217e07f8ff4
Alvin Cassar <alvin.cassar.d57451@mcast.edu.mt>
2021-10-15 11:57:15 +0200

    continue q1 ws1

--- ws1/q1.c
+++ ws1/q1.c
@@ -3,14 +3,25 @@
 
 int main(void) {
     
-    int num1;
-    int num2;
+    int num1, num2, sum;
+    float avg;
 
     printf("Please enter 1st number:");
-    scanf("%d", num1);
+    scanf("%d", &num1);
     printf("Please enter 2nd number:");
-    scanf("%d", num2);
+    scanf("%d", &num2);
 
-    
+    sum = sum(num1, num2);
+    avg = sum/2;
+
+    printf("Sum: %d \n", sum);
+    printf("Average: %.2f \n", avg);
+
     return 0;
+}
+
+float sum(int n1, int n2){
+
+    float ans = n1 + n2;
+    return ans;
 }
